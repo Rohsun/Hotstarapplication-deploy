656f44c0335247da0d6423b888c7acdc8ab96c9e
Rohsun <166361230+Rohsun@users.noreply.github.com>
2026-06-26 18:09:55 +0530

    Format App.js for consistent code style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import VideoList from './components/VideoList';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hotstar Clone</h1>
-      <VideoList />
-    </div>
-  );
+
+ return (
+   <div className="App">
+
+    <h1>Hotstar Clone</h1>
+
+    <VideoList />
+
+   </div>
+ );
+
 }
 
 export default App;
-
